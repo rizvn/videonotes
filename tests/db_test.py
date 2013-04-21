c90c152104776604e4c746143d47cb0bb2d180fd
Riz <riz247@gmail.com>
2013-04-21 18:07:39 +0100

    changed tests to use pyunit

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,33 +1,35 @@
 import app.db as db
+import unittest
 
-def test_verify_getAllVideos():
-    result = db.getAllVideos()
-    assert result
-    
-def test_getNotesForVideo():
-    result = db.getNotesForVideo(1)
-    assert True
-    
-def test_getVideoByPk():
-    result = db.getVideo(1)
-    assert result
+class DbTest(unittest.TestCase):
+    def test_verify_getAllVideos(self):
+        result = db.getAllVideos()
+        assert result
 
-def test_verify_getUserByName():
-    result = db.getUserByName('riz')
-    assert result
+    def test_getNotesForVideo(self):
+        result = db.getNotesForVideo(1)
+        assert True
 
-def test_verify_authquery():
-    result = db.authUser('riz', 'password')
-    assert result
-    
-def test_insert_note():
-    db.addNote(1, 130, 'Test note', 'Riz')
-    assert True
+    def test_getVideoByPk(self):
+        result = db.getVideo(1)
+        assert result
 
-def test_verify_delete_note():
-    db.deleteNote(2)
-    assert True
+    def test_verify_getUserByName(self):
+        result = db.getUserByName('riz')
+        assert result
 
-def test_updateNote():
-    db.updateNote(20, 'hello', 'world')
-    assert True
+    def test_verify_authquery(self):
+        result = db.authUser('riz', 'password')
+        assert result
+
+    def test_insert_note(self):
+        db.addNote(1, 130, 'Test note', 'Riz')
+        assert True
+
+    def test_verify_delete_note(self):
+        db.deleteNote(2)
+        assert True
+
+    def test_updateNote(self):
+        db.updateNote(20, 'hello', 'world')
+        assert True
