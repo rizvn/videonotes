68ba5195a40786db85b0ef00922fce28e1e701b3
Riz <riz247@gmail.com>
2013-04-21 15:18:04 +0100

    login working

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from bottle import route, post, get, run, hook, jinja2_view as view, \
-            jinja2_template as template, static_file, request
+            jinja2_template as template, static_file, request, redirect
+from threading import local
 import sqlite3
 import bottle
 import app.db as db
 import re
 import urlparse
 
-
+req = local()
 
 def loggedInCheck(fn):
     def wrap():
@@ -14,17 +15,24 @@ def loggedInCheck(fn):
         return fn()
     return wrap
 
+def view(*args, **kwargs):
+    #add user name
+    kwargs['user'] = req.session['user']
+    #render template
+    return template(*args, **kwargs)
+
 
 @hook('before_request')
 def login():
     ses = bottle.request.environ.get('beaker.session')
-    if 'user' not in ses:
-        print('Setting up user')
-        ses['user'] = 'anonymous'
-    else:
-        print ('Existing user', ses['user'])
+    excludes = ['/login', '/static/']
+    for exclude in excludes:
+        if exclude in bottle.request.url: return
 
-    print 'Running login code before request'
+    if 'user' not in ses:
+        redirect('/login')
+    else:
+        req.session = ses
 
 @route('/play/<vid_pk>')
 def player(vid_pk):
@@ -37,7 +45,7 @@ def player(vid_pk):
         match = re.match(r"(?P<yt_id>v=(\d|\w)*)", params)
         vid['url'] = match.group('yt_id')[2:]
     
-    return template('player.html',
+    return view('player.html',
                     video=vid,
                     youtube_video=youtube_video,
                     notes=db.getNotesForVideo(vid_pk))
@@ -51,7 +59,7 @@ def index():
 
 @route('/library')
 def library():
-    return template("library.html", videos = db.getAllVideos())
+    return view("library.html", videos = db.getAllVideos())
 
 @post('/note')
 def addNote():
@@ -71,6 +79,27 @@ def deleteNote(id):
 def updateNote(id):
     db.updateNote(id, request.forms.get('time'), request.forms.get('text'))
     return {'ack': 1}
+
+@get('/login')
+def login():
+    return template("login.html")
+
+@post('/login')
+def auth():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if db.authUser(username, password):
+        ses = bottle.request.environ.get('beaker.session')
+        ses['user'] = username
+        return redirect('/library')
+    else:
+        return template("login.html", error="Invalid login details")
+
+@get('/logout')
+def logout():
+    ses = bottle.request.environ.get('beaker.session')
+    ses.delete()
+    redirect("/login");
 
 @route('/static/<filepath:path>')
 def serve_static(filepath):
