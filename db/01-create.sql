5a3f390efe0504f7bd708a061ed3208aa7d3b608
Riz <riz247@gmail.com>
2013-04-27 15:03:13 +0100

    merged script into one file

--- db/01-create.sql
+++ db/01-create.sql
@@ -2,13 +2,16 @@
 create schema  videonotes;
 
 create user 'riz'@'localhost';
-use videonotes;
+
 GRANT ALL ON videonotes.* TO 'riz'@'localhost' identified by 'pass';
 
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'riz'@'localhost';
 drop user 'riz'@'localhost';
 */
-
+use videonotes;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS videos;
 
 CREATE TABLE videonotes.notes
 (
@@ -53,4 +56,19 @@ CREATE TABLE videonotes.videos
   url varchar(150) NOT NULL,
   ts timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 ) ENGINE=INNODB;
+
+
+/*------------------- Add Data -------------------------------*/
+use videonotes;
+insert  into videonotes.users (pk, username, password)
+  values ('1', 'riz', 'riz@rizvn.com', 'pass');
+
+insert  into videonotes.users (pk, username, email, password)
+  values ('2', 'bob', 'bob@rizvn.com' ,'pass');
+
+insert into videonotes.videos(pk, title, url)
+  values (1, 'Paradox of choice - Barry Scwhartz', '/static/testVideos/paradox_of_choice.mp4');
+
+insert into videonotes.videos(pk, title, url)
+  values (2, 'Paradise - Coldplay', 'http://www.youtube.com/watch?v=1G4isv_Fylg');
 
