d858440db96f97c08a845d6a5246609952572dde
Riz <riz247@gmail.com>
2013-05-04 14:01:20 +0100

    fixed validation test

--- tests/validation_test.py
+++ tests/validation_test.py
@@ -3,4 +3,5 @@ import app.validation as validation
 
 class ValidationTest(unittest.TestCase):
     def test_validate_pwd(self):
-        errors =
+        errors = validation.validate_pwd("")
+        self.assertEqual(len(errors), 1, 'Invalid number of errors')
