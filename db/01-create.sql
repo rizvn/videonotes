5829cce0177bc14c5551211eeff1c5beca3078f7
Riz <riz247@gmail.com>
2013-06-01 16:27:40 +0100

    added videos to standing data

--- db/01-create.sql
+++ db/01-create.sql
@@ -8,10 +8,13 @@ GRANT ALL ON videonotes.* TO 'riz'@'localhost' identified by 'pass';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'riz'@'localhost';
 drop user 'riz'@'localhost';
 */
+
+use videonotes;
+
 DROP TABLE IF EXISTS notes;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS videos;
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS tags;
 
 CREATE TABLE notes
 (
@@ -43,16 +46,16 @@ CREATE TABLE videos
   pk bigint  PRIMARY KEY NOT NULL AUTO_INCREMENT,
   title varchar(100) NOT NULL,
   url varchar(150) NOT NULL,
-  cat varchar(150),
+  tags varchar(150),
   created_by varchar(30),
   share int default 0,
   ts timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 ) ENGINE=INNODB;
 
-CREATE TABLE categories
+CREATE TABLE tags
 (
-  pk bigint  PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
+  pk bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL
 ) ENGINE=INNODB;
 
 
@@ -63,5 +66,26 @@ insert  into users (pk, username, email, password)
 insert  into users (pk, username, email, password)
   values ('2', 'bob', 'bob@rizvn.com' ,'pass');
 
-insert into videos(pk, title, url)
-  values (1, 'Paradise - Coldplay', 'http://www.youtube.com/watch?v=1G4isv_Fylg');
+insert into videos(pk, title, url, tags, created_by)
+    values (2,'Sell Your Ideas the Steve Jobs Way', 'http://www.youtube.com/watch?v=0q-wvAIeUgk', 'Business', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (3,'Pitch Anything in 15 seconds', 'http://www.youtube.com/watch?v=phyU2BThK4Q', 'Business', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (5,'Evaluating a business Idea', 'http://www.youtube.com/watch?v=y9ClKzMq3n0', 'Business', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (2,'Sell Your Ideas the Steve Jobs Way', 'http://www.youtube.com/watch?v=0q-wvAIeUgk', 'Business', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (3,'Pitch Anything in 15 seconds', 'http://www.youtube.com/watch?v=phyU2BThK4Q', 'Business', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (6,'Introduction to Mac OS X, Cocoa Touch, Objective-C and Tools', 'http://www.youtube.com/watch?v=oesNwgHn1ws', 'Programming', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (7,'iPhone Programming Tutorial 2 - Building a Simple Calculator', 'http://www.youtube.com/watch?v=OT_Z3qxxSsI', 'Programming', 'admin');
+
+insert into videos(pk, title, url, tags, created_by)
+    values (8,'iPhone Programming Tutorial 3 - Objective-C ', 'http://www.youtube.com/watch?v=jdCDRpgExtc', 'Programming', 'admin');
