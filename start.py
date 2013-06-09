e0e1fb593ee2078bddf3cdc4e78acf1cc827bd49
Riz <riz247@gmail.com>
2013-06-09 18:50:18 +0100

    added if statement for heroku

--- start.py
+++ start.py
@@ -12,7 +12,7 @@ session_opts = {
 }
 app = bottle.default_app()
 app = SessionMiddleware(app, session_opts)
-port = int(os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 5000))
 
 import socket
 hostname = socket.gethostname()
